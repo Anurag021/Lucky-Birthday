f0a949b0f0a5b3c967f37b839517420bc722c9e8
Anurag021 <anu.rawat@outlook.com>
2021-09-18 18:05:21 +0530

    updated Fix:
    updated lucky number logic

--- App.js
+++ App.js
@@ -17,8 +17,8 @@ function clickHandler(){
         sum = sum + (DOB % 10);
         DOB = Math.trunc(DOB/10)   
     }
-    if (sum /luckyNumber.value == 0){
-        result.innerText = "DOB is a lucky number"
+    if (sum % luckyNumber.value == 0){
+        result.innerText = "DOB is lucky number"
     }else{
         result.innerText = "DOB is not a lucky number"
     }
